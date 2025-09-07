c753a0d351590b63eff72595a14a300ab7497efa
AdroitBit <yanothaic15@gmail.com>
2025-09-07 14:30:08 +0700

    test rosdep

--- run_rosdep.py
+++ run_rosdep.py
@@ -2,4 +2,4 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 os.system('colcon build')
-os.system('rosdep install -i --from-path src --rosdistro jazzy -y')
+os.system('rosdep install -i --from-path src --ignore-src -r --rosdistro jazzy -y')
