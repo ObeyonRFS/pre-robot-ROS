fc784e055d1617528f997028769509f05770b802
AdroitBit <yanothaic15@gmail.com>
2025-09-19 21:06:43 +0700

    retest

--- src/pre_robot_robot_pc/pre_robot_robot_pc/MotorControllerWithROS/serial_comm_library.h
+++ src/pre_robot_robot_pc/pre_robot_robot_pc/MotorControllerWithROS/serial_comm_library.h
@@ -2,14 +2,19 @@
 
 #include <ArduinoJson.h>
 
+SemaphoreHandle_t serialMutex = xSemaphoreCreateMutex();
+
 void send_serial_motor_rpm_feedback(float L_rpm, float R_rpm) {
   StaticJsonDocument<1024> doc;
   doc["feedback_type"] = "motor_rpm";
   doc["data"]["L"] = L_rpm;
   doc["data"]["R"] = R_rpm;
 
-  serializeJson(doc, Serial);
-  Serial.println();
+  if (xSemaphoreTake(serialMutex, portMAX_DELAY)) {
+    serializeJson(doc, Serial);
+    Serial.println();
+    xSemaphoreGive(serialMutex);
+  }
 }
 
 void send_serial_debug_msg_feedback(const String& msg) {
@@ -17,6 +22,9 @@ void send_serial_debug_msg_feedback(const String& msg) {
   doc["feedback_type"] = "debug_msg";
   doc["data"]["msg"] = msg;
 
-  serializeJson(doc,Serial);
-  Serial.println();
+  if (xSemaphoreTake(serialMutex, portMAX_DELAY)) {
+    serializeJson(doc, Serial);
+    Serial.println();
+    xSemaphoreGive(serialMutex);
+  }
 }
