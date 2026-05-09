39623b0284b9c27a2eba4c1d3a420664a2c23d70
AdroitBit <yanothaic15@gmail.com>
2026-05-09 20:50:05 +0700

    adjust again

--- src/pre_robot_robot_pc/pre_robot_robot_pc/middleware_to_esp32.py
+++ src/pre_robot_robot_pc/pre_robot_robot_pc/middleware_to_esp32.py
@@ -162,8 +162,8 @@ class MiddlewareNode(Node):
         t.header.stamp = self.get_clock().now().to_msg()
         t.header.frame_id = "base_link"
         t.child_frame_id = "left_wheel_link"
-        t.transform.translation.x = self.x-9.7/100
-        t.transform.translation.y = self.y+self.distance_between_wheels/2
+        t.transform.translation.x = -9.7/100
+        t.transform.translation.y = +self.distance_between_wheels/2
         t.transform.translation.z = -14/100+self.wheel_radius
         q = quaternion_from_euler(0, self.left_wheel_rotation, 0)
         t.transform.rotation.x = q[0]
@@ -177,8 +177,8 @@ class MiddlewareNode(Node):
         t.header.stamp = self.get_clock().now().to_msg()
         t.header.frame_id = "base_link"
         t.child_frame_id = "right_wheel_link"
-        t.transform.translation.x = self.x-9.7/100
-        t.transform.translation.y = self.y-self.distance_between_wheels/2
+        t.transform.translation.x = -9.7/100
+        t.transform.translation.y = -self.distance_between_wheels/2
         t.transform.translation.z = -14/100+self.wheel_radius
         q = quaternion_from_euler(0, self.right_wheel_rotation, 0)
         t.transform.rotation.x = q[0]
